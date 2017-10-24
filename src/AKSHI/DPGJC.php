a071dc54f06ca29378479b9882d42ec0a13dc77b
gjergj.sheldija <gjergj.sheldija@gmail.com>
2017-10-24 12:55:35 +0200

    - code refactor - added ability to send request options

--- src/AKSHI/DPGJC.php
+++ src/AKSHI/DPGJC.php
@@ -17,141 +17,107 @@ use GovTalk;
 
 class DPGJC extends GovTalk\GovTalk
 {
+	/**
+	 * GovTalk service.
+	 *
+	 * @var string
+	 */
+	const SERVICE = "http://eDPGJC.org/";
 
-    /**
-     * GovTalk server.
-     *
-     * @var string
-     */
-    private $gatewayURL;
+	/**
+	 * GovTalk function.
+	 *
+	 * @var string
+	 */
+	const FUNCTION = "GetPersonByNID";
 
-    /**
-     * GovTalk sender userID.
-     *
-     * @var string
-     */
-    private $gatewayUserId;
-
-    /**
-     * GovTalk sender password.
-     *
-     * @var string
-     */
-    private $gatewayUserPassword;
-
-    /**
-     * GovTalk service.
-     *
-     * @var string
-     */
-    private $service = "http://eDPGJC.org/";
-
-    /**
-     * GovTalk function.
-     *
-     * @var string
-     */
-    private $function = "GetPersonByNID";
-
-    /* Public methods. */
+	/* Public methods. */
 
 
-    /**
-     * Instance constructor.
-     *
-     * @param string $govTalkSenderId    GovTalk sender ID.
-     * @param string $govTalkPassword    GovTalk password.
-     * @param string $govTalkServer      GovTalk server URL.
-     * @param string $messageLogLocation Message log location (default null = no logging)
-     */
-    public function __construct($userID, $password, $url, $messageLogLocation)
-    {
-        $this->gatewayUserId = $userID;
-        $this->gatewayUserPassword = $password;
-        $this->gatewayURL = $url;
-        $this->messageLogLocation = $messageLogLocation;
-    }
-
-    /**
-     * Service setup
-     */
-    public function setUpService()
-    {
-        parent::__construct(
-            $this->gatewayURL,
-            $this->gatewayUserId,
-            $this->gatewayUserPassword,
-            null,
-            $this->messageLogLocation
-        );
-    }
+	/**
+	 * Instance constructor.
+	 *
+	 * @param string $govTalkSenderId    GovTalk sender ID.
+	 * @param string $govTalkPassword    GovTalk password.
+	 * @param string $govTalkServer      GovTalk server URL.
+	 * @param string $messageLogLocation Message log location (default null = no logging)
+	 */
+	public function __construct($userID, $password, $url, $messageLogLocation)
+	{
+		parent::__construct(
+			$url,
+			$userID,
+			$password,
+			null,
+			$messageLogLocation
+		);
+	}
 
 
-    /**
-     * Constructes the message based on AKSHI requirements for the eDPGJC
-     * specification.
-     * Constructed the SOAP body manualy to avoid consuming resources by
-     * calling \SOAPClient
-     *
-     * @param $NID string the The persons NID for which the query is being made
-     */
-    public function constructMessage($NID)
-    {
-        $this->gtService = $this->setUpService();
 
-        $this->setMessageAuthentication('clear');
-        $this->addMessageKey('SpokeName', 'SVC_6_IND');
-        $this->setMessageClass('TRA_GETPERSONALDATA');
-        $this->setMessageQualifier('request');
-        $this->setMessageFunction('submit');
+	/**
+	 * Constructes the message based on AKSHI requirements for the eDPGJC
+	 * specification.
+	 * Constructed the SOAP body manualy to avoid consuming resources by
+	 * calling \SOAPClient
+	 *
+	 * @param $NID string the The persons NID for which the query is being made
+	 */
+	public function constructMessage($NID)
+	{
+		$this->setMessageAuthentication('clear');
+		$this->addMessageKey('SpokeName', 'SVC_6_IND');
+		$this->setMessageClass('TRA_GETPERSONALDATA');
+		$this->setMessageQualifier('request');
+		$this->setMessageFunction('submit');
 
-        $package = new \XMLWriter();
-        $package->openMemory();
-        $package->setIndent(true);
+		$package = new \XMLWriter();
+		$package->openMemory();
+		$package->setIndent(true);
 
-        $package->startElement('soap12:Envelope');
-        $package->writeAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
-        $package->writeAttribute('xmlns:xsd', 'http://www.w3.org/2001/XMLSchema');
-        $package->writeAttribute('xmlns:soap12', 'http://www.w3.org/2003/05/soap-envelope');
-        $package->startElement('soap12:Body');
-        $package->startElement($this->function);
-        $package->writeAttribute('xmlns', $this->service);
-        $package->writeElement('V_NID', $NID);
-        $package->endElement();
-        $package->endElement();
-        $package->endElement();
-        $this->setMessageBody($package->outputMemory());
-    }
+		$package->startElement('soap12:Envelope');
+		$package->writeAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+		$package->writeAttribute('xmlns:xsd', 'http://www.w3.org/2001/XMLSchema');
+		$package->writeAttribute('xmlns:soap12', 'http://www.w3.org/2003/05/soap-envelope');
+		$package->startElement('soap12:Body');
+		$package->startElement(self::FUNCTION);
+		$package->writeAttribute('xmlns', self::SERVICE);
+		$package->writeElement('V_NID', $NID);
+		$package->endElement();
+		$package->endElement();
+		$package->endElement();
+		$this->setMessageBody($package->outputMemory());
+	}
 
-    /**
-     * @param bool $schemaValidation Enable or disable the schema validation. Default diabled
-     * because the http://www.govtalk.gov.uk is not online anymore
-     * @return array Return array with given data or error message
-     */
-    public function sendQuery($schemaValidation = false)
-    {
+	/**
+	 * @param bool $schemaValidation Enable or disable the schema validation. Default diabled
+	 * because the http://www.govtalk.gov.uk is not online anymore
+	 * @return array Return array with given data or error message
+	 */
+	public function sendQuery($schemaValidation = false)
+	{
 
-        if ($schemaValidation == false) {
-            $this->setSchemaValidation(false);
-        }
+		if ($schemaValidation == false) {
+			$this->setSchemaValidation(false);
+		}
 
-        if ($this->sendMessage() && ($this->responseHasErrors() === false)) {
-            $returnable = $this->getResponseEndpoint();
-            $returnable['transactionId'] = $this->getTransactionId();
-            $returnable['correlationId'] = $this->getResponseCorrelationId();
-            $this->fullResponseObject->registerXPathNamespace('person', $this->service);
-            $person = $this->fullResponseObject->xpath('//person:Table');
-            foreach ($person as $node) {
-                $array = array();
-                foreach ($node->children() as $child) {
-                    $array[$child->getName()] = (string)$child;
-                }
-                $returnable['statusRecords'] = $array;
-            }
-        } else {
-            $returnable = array('errors' => $this->getResponseErrors());
-        }
+		if ($this->sendMessage() && ($this->responseHasErrors() === false)) {
+			$returnable = $this->getResponseEndpoint();
+			$returnable['transactionId'] = $this->getTransactionId();
+			$returnable['correlationId'] = $this->getResponseCorrelationId();
+			$this->fullResponseObject->registerXPathNamespace('person', self::SERVICE);
+			$person = $this->fullResponseObject->xpath('//person:Table');
+			foreach ($person as $node) {
+				$array = array();
+				foreach ($node->children() as $child) {
+					$array[$child->getName()] = (string)$child;
+				}
+				$returnable['statusRecords'] = $array;
+			}
+		} else {
+			$returnable = array('errors' => $this->getResponseErrors());
+		}
 
-        return $returnable;
-    }
+		return $returnable;
+	}
 }
